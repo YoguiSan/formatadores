11a6a3853883eb22b43add787cf264e3bb0ef14d
Nelson Salles de Barros Filho <nelsonsbf@outlook.com>
2021-05-04 12:42:42 -0300

    Implementado script de gerar versão

---
+++ gerar-versao.js
@@ -0,0 +1,36 @@
+#!/usr/bin/env node
+/* eslint-disable import/no-dynamic-require */
+
+const fs = require('fs');
+
+let packageJsonUrl = '../../package.json';
+let metaJsonUrl = './public/meta.json';
+
+process.argv.map((arg) => {
+  if (arg.indexOf('--metaJsonUrl=') > -1) {
+    metaJsonUrl = arg.replace('--metaJsonUrl=', '');
+  }
+  if (arg.indexOf('--packageJsonUrl=') > -1) {
+    packageJsonUrl = arg.replace('--packageJsonUrl=', '');
+  }
+  return packageJsonUrl;
+});
+
+const packageJson = require(packageJsonUrl);
+
+const appVersion = packageJson.version;
+
+const jsonData = {
+  version: appVersion,
+};
+
+const jsonContent = JSON.stringify(jsonData);
+
+fs.writeFile(metaJsonUrl, jsonContent, 'utf8', (err) => {
+  if (err) {
+    console.log('Ocorreu um erro ao gerar o arquivo meta.json');
+    return console.log(err);
+  }
+
+  return console.log(`meta.json salvo com o número da versão mais recente ${(appVersion)}`);
+});
