f0668873a2f9978f9258693b519c3966f47e9b52
Nelson Salles de Barros Filho <nelsonsbf@outlook.com>
2021-10-19 16:50:44 -0300

    fix(rollup.config): arrumadas configurações do Rollup

--- rollup.config.js
+++ rollup.config.js
@@ -3,8 +3,7 @@ import resolve from 'rollup-plugin-node-resolve';
 import commonjs from 'rollup-plugin-commonjs';
 import external from 'rollup-plugin-peer-deps-external';
 import json from '@rollup/plugin-json';
-// import postcss from 'rollup-plugin-postcss'
-// import url from '@rollup/plugin-url'
+
 import pkg from './package.json';
 
 const extensions = ['.js', '.jsx', '.ts', '.tsx'];
@@ -24,27 +23,13 @@ export default [
     }],
     plugins: [
       external(),
-      // url({
-      //   include: [
-      //     '**/*.svg',
-      //     '**/*.png',
-      //     '**/*.jpg',
-      //     '**/*.gif',
-      //     '**/*.woff',
-      //     '**/*.woff2',
-      //     '**/*.ttf'
-      //   ],
-      // }),
-      // postcss({
-      //   extract: false
-      // }),
       babel({
         exclude: 'node_modules/**',
         presets: ['@babel/env', '@babel/preset-react'],
       }),
       resolve({ extensions }),
       json({
-        exclude: ['node_modules/**'],
+        // exclude: ['node_modules/**'],
         namedExports: true,
         compact: true,
         preferConst: true,
